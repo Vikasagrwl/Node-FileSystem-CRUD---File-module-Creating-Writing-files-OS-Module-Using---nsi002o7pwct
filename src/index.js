0e72a73b676ff037de85ea15cbc69c546d6c33f1
Vikasagrwl <105685053+Vikasagrwl@users.noreply.github.com>
2023-04-17 09:33:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,22 +3,27 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	fs.writeFile(fileName, fileContent);
 }
 
 const myFileReader = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	const content = fs.readFile(fileName, 'utf8');
+	return content;
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	fs.appendFile(fileName, fileContent);
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	fs.unlink(fileName)
 }
 
 
